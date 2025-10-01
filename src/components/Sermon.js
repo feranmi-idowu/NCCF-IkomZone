0701210e31a2e15cdb3a76c93ff3f3d2c640e019
feranmi-idowu <idowuferanmi4@gmail.com>
2025-10-01 11:54:47 +0100

    upgrade on sermon

--- src/components/Sermon.js
+++ src/components/Sermon.js
@@ -3,16 +3,51 @@ import React from "react";
 function Sermons() {
   return (
     <section id="sermons">
-      <div className="container">
-        <h2>Sermons</h2>
-        <p>Listen to inspiring messages from our services.</p>
-        <audio controls>
-          <source
-            src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
-            type="audio/mpeg"
-          />
-          Your browser does not support the audio element.
-        </audio>
+      <div>
+        <div className="containerAudio">
+          <h2>Sermons</h2>
+          <p>Listen to inspiring messages from our services.</p>
+          <div className="containerAudio">
+            <h4>Sermon's Title</h4>
+            <audio controls>
+                <source
+                    src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+                    type="audio/mpeg"
+                />
+                Your browser does not support the audio element.
+            </audio>
+          </div>
+          <div className="containerAudio">
+            <h4>Sermon's Title</h4>
+            <audio controls>
+                <source
+                    src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+                    type="audio/mpeg"
+                />
+                Your browser does not support the audio element.
+            </audio>
+          </div>
+          <div className="containerAudio">
+            <h4>Sermon's Title</h4>
+            <audio controls>
+                <source
+                    src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+                    type="audio/mpeg"
+                />
+                Your browser does not support the audio element.
+            </audio>
+          </div>
+          <div className="containerAudio">
+            <h4>Sermon's Title</h4>
+            <audio controls>
+                <source
+                    src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+                    type="audio/mpeg"
+                />
+                Your browser does not support the audio element.
+            </audio>
+          </div>
+        </div>
       </div>
     </section>
   );
