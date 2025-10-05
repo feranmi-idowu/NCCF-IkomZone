a73c469b1bffda342f0f39fe5af783d47d33cc2a
feranmi-idowu <idowuferanmi4@gmail.com>
2025-10-05 23:37:46 +0100

    gallery height & pic

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -7,19 +7,19 @@ function Gallery() {
         <h2>Gallery</h2>
         <div className="gallery">
           <img
-            src=""
+            src="https://scontent.fabb1-3.fna.fbcdn.net/v/t39.30808-6/558098168_122150807012672617_5969214760706273607_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeEq4bBDUZkTcY5a8aZiPuiZ28NDRGjwQaPbw0NEaPBBo4637BvK8RXfeUFcRj0GlKQ8pPfWdwQRdCGYoH8-fKKh&_nc_ohc=YY7cB_KE_tMQ7kNvwGaBvkq&_nc_oc=AdmgVicrV2NtHQy0fcJTbhogG1FVh6-gi4QB3pxsybMw2kfQC1XjBXaTUVdb4ltr0Kw&_nc_zt=23&_nc_ht=scontent.fabb1-3.fna&_nc_gid=jRzNPWboj_e09kFiyBvMIQ&oh=00_AffeDcfglO57XiF8NoP9K3VqHwMK6gGDq8C_IVjqrpgS7w&oe=68E8ACAE"
             alt="Gallery 1"
           />
           <img
-            src=""
+            src="https://scontent.fabb1-3.fna.fbcdn.net/v/t39.30808-6/556134110_122150806262672617_146843291106953578_n.jpg?stp=dst-jpg_s600x600_tt6&_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeESD2NxUvvxtbaF9j3YYMWmolyeorx1tmWiXJ6ivHW2ZchyvlfuVU_vm5bHvVcfedbhiWumt2x1MD6FIipatnBY&_nc_ohc=cKM1LNAtgKMQ7kNvwGlnH8z&_nc_oc=AdkvEk3ihwjJgywL2_Ad4mym5Fywt4dqZQFfgQbV2fg3PsKQesrB9Cpce1OeFuM1NXw&_nc_zt=23&_nc_ht=scontent.fabb1-3.fna&_nc_gid=2nHI7vIAXkqal9oodHxLIw&oh=00_AfdbTvQmao7Yu0ezLRWN7ufoqBI10dKYIKyj4hYkRgIfLg&oe=68E8D7C5"
             alt="Gallery 2"
           />
           <img
-            src=""
+            src="https://scontent.fabb1-3.fna.fbcdn.net/v/t39.30808-6/556531544_122150806334672617_1036555303794947823_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeHfZ0mdED9KwBjumbUHFblvV-eS7VzlJ4FX55LtXOUngcflFgv6fQhBxXEgmNJ3kEBhoiHz5DMFXfXQFm287RKK&_nc_ohc=SJmu4cnaURQQ7kNvwHcIFKY&_nc_oc=AdljfOa-kjs280eKWo2mqcVML_7FfLGSjl3Fodml_nVlVwNo0ez6e2cXPBkMuwYyWC0&_nc_zt=23&_nc_ht=scontent.fabb1-3.fna&_nc_gid=HBhQo_2Sauo1BfL61jO-hQ&oh=00_AfeZYE8iwo2BL6FZy37llrx4zYCzJgtnaLqcfymMBUdviA&oe=68E8CD31"
             alt="Gallery 3"
           />
           <img
-            src=""
+            src="https://scontent.fabb1-2.fna.fbcdn.net/v/t39.30808-6/556132937_122150806718672617_3781819153464939110_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFso5ASp79B960_g2VYE3OhsPtQ5Mmw8Pyw-1DkybDw_Azro8IZUzlgv8S81QRwijd9JiRPPUaBIhpjPdkfc_m6&_nc_ohc=r9mwcMmzMfEQ7kNvwFq-yFU&_nc_oc=AdmOY1BcC8qRcz5SvcYGpvsmBuAJ7pR0dXOhybEJtYVJt7D6TMEj7x6aLnEwKG11VOU&_nc_zt=23&_nc_ht=scontent.fabb1-2.fna&_nc_gid=y_130cnN76LrFcebiI-uWg&oh=00_AfcHxXiZFRYQv_Ty5WLRz1WxOhy92H0LR4WaSoeWe_RWCQ&oe=68E8B647"
             alt="Gallery 4"
           />
         </div>
