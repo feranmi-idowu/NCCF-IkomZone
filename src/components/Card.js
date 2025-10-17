611992a8d1c69da9848172f3bb537ad91388c178
feranmi-idowu <idowuferanmi4@gmail.com>
2025-10-17 21:58:02 +0100

    card component

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Card.css";
+
+function Card({ title, description, icon }) {
+  return (
+    <div className="card">
+      {icon && <div className="card-icon">{icon}</div>}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default Card;
