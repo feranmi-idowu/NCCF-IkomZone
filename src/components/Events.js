c88e8aff5d97357490a00f4ff44bf4a13c3f364f
feranmi-idowu <idowuferanmi4@gmail.com>
2025-10-19 23:12:06 +0100

    updated events to props

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,6 +1,44 @@
 import React from "react";
 
+import Card from "./Card";
+
 function Events() {
+  const events = [
+    {
+      title: "Crossover Night",
+      description: "Join us for a night of worship, prayer, and thanksgiving.",
+    },
+    {
+      title: "Rural Rugged Evangelism",
+      description: "Taking the gospel to the villages around Ikom.",
+    },
+    {
+      title: "Night of Encounter",
+      description: "A powerful praise and prayer vigil with diverse ministrations.",
+    },
+  ];
+
+  return (
+    <section id="programs">
+      <div className="container">
+        <h2>Upcoming Events</h2>
+        <div className="cards">
+          {events.map((program, index) => (
+            <Card
+              key={index}
+              title={program.title}
+              description={program.description}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+
+
+/* function Events() {
   return (
     <section id="events">
       <div className="container">
@@ -23,5 +61,5 @@ function Events() {
     </section>
   );
 }
-
+*/
 export default Events;
