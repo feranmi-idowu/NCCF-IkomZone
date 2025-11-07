c78aa4f33096d64a71125c725a6a8ee497e470b6
feranmi-idowu <idowuferanmi4@gmail.com>
2025-11-07 23:41:53 +0100

    page styles

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function About() {
     return (
-        <section id="about">
+        <section id="about" className="container">
             <h2>About Us</h2>
             <p>We are a family of young believers of Jesus, discipled in the Word, 
         and sent to serve our host communities across Ikom and beyond</p>
