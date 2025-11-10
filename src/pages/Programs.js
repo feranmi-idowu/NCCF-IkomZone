4dd1959e3a44e5bbc7b5892dd83d66b94e66dafe
feranmi-idowu <idowuferanmi4@gmail.com>
2025-11-11 00:02:39 +0100

    prg

--- src/pages/Programs.js
+++ src/pages/Programs.js
@@ -1,31 +1,5 @@
 import React, { useRef } from "react";
 
-/* function Programs() {
-  return (
-    <section id="programs">
-      <div className="container">
-        <h2>Our Programs</h2>
-        <div className="cards">
-          <div className="card">
-            <h3>Sunday Glorious Service</h3>
-            <p>Join us every Sunday for worship and word ministration.</p>
-          </div>
-          <div className="card">
-            <h3>Bible Study</h3>
-            <p>Weekly in-depth study of God’s Word for spiritual growth.</p>
-          </div>
-          <div className="card">
-            <h3>Prayer Meetings</h3>
-            <p>Intense moments of prayer, intercession, and encounters.</p>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-}
-
-export default Programs; */
-
 
 import Card from "../components/Card";
 
