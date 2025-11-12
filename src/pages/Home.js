955e9dd88156e93b07c4aecc6163b0ee18430ca2
feranmi-idowu <idowuferanmi4@gmail.com>
2025-11-12 23:49:45 +0100

    homecard

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,10 +7,12 @@ function Home() {
             <div>
                 <img src={IMG_7683} alt="worship"/>
             </div>
-            <div className="content">
-                <h1>Raising a Godly Generation</h1>
-                <p>NCCF Ikom Zone exist to build corps member in Christ and impact our host community</p>
-                <a href="#events">Join Our Events</a>
+            <div className="cardHome">
+              <div className="content">
+                  <h1>Raising a Godly Generation</h1>
+                  <p><b>NCCF Ikom Zone exist to build corps member in Christ and impact our host community</b></p>
+                  <a href="#events">Join Our Events</a>
+              </div>
             </div>
         </section>
     );
